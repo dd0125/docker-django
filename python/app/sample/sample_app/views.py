02085e28307edba28899a5370ecf356619990196
dd0125 <dd.0125.jp@gmail.com>
2019-06-23 09:55:09 +0900

    template を使ってみた

--- python/app/sample/sample_app/views.py
+++ python/app/sample/sample_app/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     #とりあえず文字列を返す。
-    return HttpResponse("Indexです！")
+#    return HttpResponse("Indexです！")
+  variable1 = 100
+  return render(request, 'sample_app/index.html',{'a':variable1})
 
 def members(request):
-	return HttpResponse("members")
+  return HttpResponse("members")
